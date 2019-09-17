5f4d0f5fe6887c25d8b23fb9c4f439038d835fc4
zhout <rayzhou.cn@hotmail.com>
2019-09-17 17:48:25 +0800

    tmp

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -13,9 +13,31 @@
         <el-col>
           <span>(右侧通常会有上下滚动栏,120 + 16左右就会消失)</span>
         </el-col> -->
-        <el-form>
+        <div class='login-box'>
+          <div class='login-header'></div>
+          <div class='login-body'>
+            <el-form>
+              <el-form-item>
+                <el-input type="text" placeholder="电话号码"/>
+              </el-form-item>
+              <el-form-item>
+                <div style="float: left">
+                  <el-input type="text" placeholder="验证码" style="width: 200px;"/>
+                  <el-button style="margin-left: 33px;">发送验证码</el-button>
+                </div>
+              </el-form-item>
+              <el-form-item>
+                <el-button type="primary" v-on:click="onSubmit('loginForm')" style="width: 100%;">登录</el-button>
+              </el-form-item>
+              <el-form-item>
+                <div style="float: left; color: blue;">
+                    <span>用户名密码登录</span>
+                </div>
+              </el-form-item>  
+            </el-form>
+          </div>
+        </div>
 
-        </el-form>
       </el-main>
     <!-- </el-container> -->
     <el-footer>Footer 默认 height: 60px;</el-footer>
@@ -43,14 +65,19 @@
     text-align: center;
     line-height: 160px;
   }
-  .el-form {
-      background-color: red;
+  .login-box {
+      /* background-color: red; */
       height: 300px;
       width: 350px;
       margin: 0 auto; /* 水平居中 */
       position: relative; /*脱离文档流*/
       top: 50%; /*绝对偏移*/
       margin-top: -150px; /*相对偏移*/
+  }
+
+  .login-header {
+    background-color: aquamarine;
+    height: 100px;
   }
 
   /* 去掉浏览器右侧滚动条 */
